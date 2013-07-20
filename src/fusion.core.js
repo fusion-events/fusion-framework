34a4980d22bf86717b6385ff058e46a511924766
Aaron Scherer <aequasi@gmail.com>
2013-07-20 05:29:47 +0000

    Getting ready

---
+++ src/fusion.core.js
@@ -0,0 +1,16 @@
+( function( $ ) {
+
+	// Fusion Container
+	$.fn.fusion = {
+		version: '0.1'
+	};
+
+	/**
+	 *
+	 *
+	 */	
+	$.fn.fusion.fuse = function( event, selector, priority, handler ) {
+		
+	}
+
+}( jQuery ) );
