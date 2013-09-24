c9a5ea811a3f30d08e8baa706d81ab9996f32045
Aaron Scherer <aequasi@gmail.com>
2013-09-24 14:16:04 -0700

    Changing scope up. Adding models in local scope creators

--- fusion.js
+++ fusion.js
@@ -1,45 +1,67 @@
-( function ($) {
-	var
+/**
+ * @author Aaron Scherer <aequasi@gmail.com>
+ * @date 9/24/13
+ * @copyright Aaron Scherer 2013
+ */
+( function( $ ) {
+	var ctor = function() {},
+		fusion = {
+			version: '1.0.0'
+		},
 		/**
-		 * @constructor
+		 *
+		 * @param parent
+		 * @param protoProps
+		 * @param staticProps
+		 * @returns {jQuery.extend|*}
 		 */
-		ctor = function () {},
-		inherits = function (parent, protoProps, staticProps) {
+		inherits = function( parent, protoProps, staticProps ) {
 			var child;
-			if (protoProps && protoProps.hasOwnProperty('constructor')) {
+			if( protoProps && protoProps.hasOwnProperty( 'constructor' ) ) {
 				child = protoProps.constructor;
 			} else {
-				child = function () {
-					return parent.apply(this, arguments);
+				child = function() {
+					return parent.apply( this, arguments );
 				};
 			}
-			child = $.extend(true, {}, child, parent);
+			child = $.extend( true, {}, child, parent );
 			ctor.prototype = parent.prototype;
 			child.prototype = new ctor();
-			if (protoProps) child.prototype = $.extend(true, {}, child.prototype, protoProps);
-			if (staticProps) child = $.extend(true, {}, child, staticProps);
-			child.prototype.constructor = child;
-			child.__super__ = parent.prototype;
-			return child;
+			if( protoProps ) {
+				child.prototype = $.extend( true, {}, child.prototype, protoProps );
+			}
+			if( staticProps ) {
+				child = $.extend( true, {}, child, staticProps );
+			}
+				child.prototype.constructor = child;
+				child.__super__ = parent.prototype;
+				return child;
 		},
-		fusion = {
-			version: '0.1'
+		/**
+	 *
+	 * @param protoProps
+	 * @param staticProps
+	 * @returns {jQuery.extend|*}
+	 */
+		extendThis = function( protoProps, staticProps ) {
+			var child = inherits( this, protoProps, staticProps );
+			child.extend = extendThis;
+			return child;
 		};
 
-	function extendThis(protoProps, staticProps) {
-		var child = inherits(this, protoProps, staticProps);
-		child.extend = extendThis;
-		return child;
-	}
+	/************************* Models *********************************/
 
-	(function (fusion) {
+	/**
+	 * fusion.event construction
+	 */
+	(function( fusion ) {
 		/**
 		 * @constructor
 		 */
-		var func = function () {
+		ctor = function() {
 			this.data = {};
 
-			for (var arg in arguments[ 0 ]) {
+			for( var arg in arguments[ 0 ] ) {
 				this.data[ arg ] = arguments[ 0 ][ arg ];
 			}
 
@@ -47,10 +69,10 @@
 
 		};
 
-		fusion.event = new func();
+		fusion.event = new ctor();
 
-		fusion.event.__proto__.initialize = function( ) {
-			for (var arg in arguments[ 0 ]) {
+		fusion.event.__proto__.initialize = function() {
+			for( var arg in arguments[ 0 ] ) {
 				if( this.prototype[ arg ] !== undefined ) {
 					this.data[ arg ] = arguments[ 0 ][ arg ];
 				}
@@ -62,17 +84,17 @@
 		};
 
 		fusion.event.extend = extendThis;
-	})(fusion);
-
+	})( fusion );
 
 	/**
-	 * @constructor
+	 * fusion.fused construction
 	 */
-	fusion.fused = function () {
-		/**
-		 * @type {Array}
-		 */
-		this.collection = [];
+	(function( fusion ) {
+		ctor = function() {
+			this.collection = [];
+		};
+
+		fusion.fused = new ctor();
 
 		/**
 		 *
@@ -81,22 +103,22 @@
 		 * @param priority
 		 * @returns {fusion.fused}
 		 */
-		this.add = function (eventName, handler, priority) {
-			if (eventName === undefined) {
-				throw Error("eventName must be defined.");
+		fusion.fused.__proto__.add = function( eventName, handler, priority ) {
+			if( eventName === undefined ) {
+				throw Error( "eventName must be defined." );
 			}
-			if (handler === undefined) {
-				throw Error("handler must be defined.");
+			if( handler === undefined ) {
+				throw Error( "handler must be defined." );
 			}
-			if (priority === undefined) {
-				throw Error("priority must be defined.");
+			if( priority === undefined ) {
+				throw Error( "priority must be defined." );
 			}
 
-			this.collection.push({
-				name: eventName,
-				priority: priority,
-				handler: handler
-			});
+			this.collection.push( {
+				                      name:     eventName,
+				                      priority: priority,
+				                      handler:  handler
+			                      } );
 
 			return this;
 		};
@@ -108,7 +130,7 @@
 		 * @param [priority]
 		 * @returns {boolean}
 		 */
-		this.remove = function( eventName, handler, priority ) {
+		fusion.fused.__proto__.remove = function( eventName, handler, priority ) {
 			/**
 			 * @type {Array}
 			 */
@@ -116,12 +138,16 @@
 
 			for( i in this.collection ) {
 				event = this.collection[ i ];
-				if( event.name != eventName ) continue;
+				if( event.name != eventName ) {
+					continue;
+				}
 
 				if( handler === undefined ) {
 					removed.push( this.collection.splice( i, 1 ) );
 				} else if( handler == event.handler ) {
-					if( priority != event.priority ) continue;
+					if( priority != event.priority ) {
+						continue;
+					}
 					removed.push( this.collection.splice( i, 1 ) );
 				}
 			}
@@ -133,28 +159,78 @@
 		 * @param eventName
 		 * @returns {Array}
 		 */
-		this.all = function (eventName) {
-			if (eventName === undefined) {
-				throw Error("eventName must be defined.");
+		fusion.fused.__proto__.all = function( eventName ) {
+			if( eventName === undefined ) {
+				throw Error( "eventName must be defined." );
 			}
 
 			var returnCollection = [], item, i;
-			for (i in this.collection) {
+			for( i in this.collection ) {
 				item = this.collection[ i ];
-				if (item.name == eventName) {
-					returnCollection.push(item);
+				if( item.name == eventName ) {
+					returnCollection.push( item );
 				}
 			}
 
-			returnCollection.sort(function (a, b) {
-				if (a.priority > b.priority) return 1;
+			returnCollection.sort( function( a, b ) {
+				if( a.priority > b.priority ) {
+					return 1;
+				}
 				return -1;
-			});
+			} );
 
 			return returnCollection;
-		}
-	};
-	fusion.fused = new fusion.fused();
+		};
+	})( fusion );
+
+	/**
+	 * fusion.events construction
+	 */
+	(function( fusion ) {
+		ctor = function() {
+			this.collection = [];
+		};
+
+		fusion.events = new ctor();
+
+		/**
+		 *
+		 * @param eventName
+		 * @param [eventClass={}]
+		 * @returns {fusion.events}
+		 */
+		fusion.events.__proto__.add = function( eventName, eventClass ) {
+			if( this.collection[ eventName ] !== undefined ) {
+				throw Error( "Event with name '" + eventName + "' already exists!" );
+			}
+			eventClass = undefined === eventClass ? {} : eventClass;
+
+			this.collection[ eventName ] = eventClass;
+
+			return this;
+		};
+
+		/**
+		 * @param eventName
+		 * @returns {boolean}
+		 */
+		fusion.events.__proto__.has = function( eventName ) {
+			return this.collection[ eventName ] !== undefined;
+		};
+
+		/**
+		 *
+		 * @param eventName
+		 * @returns {*}
+		 */
+		fusion.events.__proto__.get = function( eventName ) {
+			return this.collection[ eventName ];
+		};
+
+
+	})( fusion );
+
+	/*********************************** Functions *******************************/
 
 	/**
 	 *
@@ -164,16 +240,16 @@
 	 * @param handler code to execute
 	 * @param [priority] int Priority to run
 	 */
-	fusion.fuse = function (eventName, handler, priority) {
-		if (fusion.events.get(eventName) === undefined) {
-			throw Error("Fused event does not exist.");
+	fusion.fuse = function( eventName, handler, priority ) {
+		if( fusion.events.get( eventName ) === undefined ) {
+			throw Error( "Fused event does not exist." );
 		}
 
-		if (priority === null || priority === undefined) {
+		if( priority === null || priority === undefined ) {
 			priority = 50;
 		}
 
-		fusion.fused.add(eventName, handler, priority);
+		fusion.fused.add( eventName, handler, priority );
 	};
 
 	/**
@@ -185,11 +261,11 @@
 	 */
 	fusion.defuse = function( eventName, handler, priority ) {
 
-		if ( !fusion.events.has(eventName) ) {
-			throw Error("Fused event does not exist.");
+		if( !fusion.events.has( eventName ) ) {
+			throw Error( "Fused event does not exist." );
 		}
 
-		if (priority === null || priority === undefined) {
+		if( priority === null || priority === undefined ) {
 			priority = 50;
 		}
 
@@ -203,9 +279,9 @@
 	 * @param eventName
 	 * @param event
 	 */
-	fusion.ignite = function (eventName, event) {
-		var events = fusion.fused.all(eventName ), eventClass, i;
-		for (i in events) {
+	fusion.ignite = function( eventName, event ) {
+		var events = fusion.fused.all( eventName ), eventClass, i;
+		for( i in events ) {
 			if( !fusion.events.has( eventName ) ) {
 				throw new Error( eventName + " is not a valid event." );
 			}
@@ -226,67 +302,21 @@
 		return true;
 	};
 
-	/**
-	 * @constructor
-	 */
-	fusion.events = function () {
-		/**
-		 * @type {Object}
-		 */
-		this.collection = {};
-
-		/**
-		 *
-		 * @param eventName
-		 * @param [eventClass={}]
-		 * @returns {fusion.events}
-		 */
-		this.add = function ( eventName, eventClass ) {
-			if (this.collection[ eventName ] !== undefined) {
-				throw Error("Event with name '" + eventName + "' already exists!");
-			}
-			eventClass = undefined === eventClass ? {} : eventClass;
-
-			this.collection[ eventName ] = eventClass;
-
-			return this;
-		};
-
-		/**
-		 * @param eventName
-		 * @returns {boolean}
-		 */
-		this.has = function( eventName ) {
-			return this.collection[ eventName ] !== undefined;
-		};
-
-		/**
-		 *
-		 * @param eventName
-		 * @returns {*}
-		 */
-		this.get = function( eventName ) {
-			return this.collection[ eventName ];
-		};
-	};
-
-	fusion.events = new fusion.events();
-
 	$.fn.fusion = fusion;
 	window.$f = ( window.$f === undefined ) ? $.fn.fusion : window.$f;
 
-	$(function () {
+	$( function() {
 		fusion.ignite( 'DOCUMENT_LOAD', { window: window, cheese: 'cheese' } );
-		$( document ).unload( function( ) {
+		$( document ).unload( function() {
 			fusion.ignite( 'DOCUMENT_UNLOAD', { window: window } );
 		} )
-	});
-}(jQuery) );
+	} );
+}( jQuery ) );
 
 /**
- * @author Aaron Scherer <aaron@undergroundelephant.com>
+ * @author Aaron Scherer <aequasi@gmail.com>
  * @date 9/24/13
- * @copyright Underground Elephant 2013
+ * @copyright Aaron Scherer 2013
  */
 ( function( fusion ) {
 
